af7be2f76f3f4711b36e794ea26799e15ecee147
Cesar Jimenez <cijgr@hotmail.com>
2019-10-22 11:49:07 -0500

    Exercise 4.5 step3 COMPLETED

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -20,7 +20,36 @@ const totalLikes = blogs => {
   }
 }
 
+const favoriteBlog = blogs => {
+  let maxLikes = 0;
+  let favorite = { title: '', author: '', likes: 0 }
+
+  if (blogs.length === 1) {
+    favorite = {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes
+    }
+  }
+
+  if (blogs.length > 1) {
+    blogs.map(blog => {
+      if (blog.likes >= maxLikes) {
+        maxLikes = blog.likes
+        favorite = {
+          title: blog.title,
+          author: blog.author,
+          likes: blog.likes
+        }
+      }
+    })
+  }
+
+  return favorite;
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
